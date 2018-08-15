55e5b862cd051d1e5f28ec57e54783c222d0773a
mo <info@norant.be>
2018-08-15 16:23:19 +0200

    Add project files.

---
+++ Gamedev/Game1.cs
@@ -0,0 +1,83 @@
+﻿using Gamedev.Assets;
+using Gamedev.Assets.Players;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Unity;
+
+namespace Gamedev
+{
+
+    public class Game1 : Game
+    {
+        GraphicsDeviceManager graphics;
+        SpriteBatch spriteBatch;
+        Player player;
+        
+        
+        public Game1()
+        {
+            graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+            graphics.PreferredBackBufferHeight = 1080;
+            graphics.PreferredBackBufferWidth = 1920;
+          //  graphics.IsFullScreen = true;
+
+        }
+
+        protected override void Initialize()
+        {
+            base.Initialize();
+        }
+
+        /// <summary>
+        /// LoadContent will be called once per game and is the place to load
+        /// all of your content.
+        /// </summary>
+        protected override void LoadContent()
+        {
+            // Create a new SpriteBatch, which can be used to draw textures.
+            spriteBatch = new SpriteBatch(GraphicsDevice);
+            player = new Player(Content);
+            
+        }
+
+        /// <summary>
+        /// UnloadContent will be called once per game and is the place to unload
+        /// game-specific content.
+        /// </summary>
+        protected override void UnloadContent()
+        {
+            // TODO: Unload any non ContentManager content here
+        }
+
+        /// <summary>
+        /// Allows the game to run logic such as updating the world,
+        /// checking for collisions, gathering input, and playing audio.
+        /// </summary>
+        /// <param name="gameTime">Provides a snapshot of timing values.</param>
+        protected override void Update(GameTime gameTime)
+        {
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+                Exit();
+
+            player.Update(gameTime);
+
+            base.Update(gameTime);
+        }
+
+        /// <summary>
+        /// This is called when the game should draw itself.
+        /// </summary>
+        /// <param name="gameTime">Provides a snapshot of timing values.</param>
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.CornflowerBlue);
+
+            player.Draw(gameTime, spriteBatch);
+
+            base.Draw(gameTime);
+        }
+    }
+}
---
+++ Gamedev/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Unity;
+
+namespace Gamedev
+{
+    /// <summary>
+    /// The main class.
+    /// </summary>
+    public static class Program
+    {
+
+        [STAThread]
+        static void Main()
+        {
+
+            using (var game = new Game1())
+                game.Run();
+        }
+    }
+}
